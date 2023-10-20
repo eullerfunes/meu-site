22e00d5a3c630d9bbf994ee674e22a88936a7b96
eullerfunes <128399799+eullerfunes@users.noreply.github.com>
2023-10-20 20:53:19 -0300

    wpp add

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
+let wpp = document.getElementById('whatsappButton')
+
+
 
 btnMenu.addEventListener("click", ()=>{
     menu.classList.add('abrir-menu')
@@ -14,3 +17,10 @@ menu.addEventListener("click", ()=>{
 overlay.addEventListener("click", ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+document.getElementById("whatsappButton").addEventListener("click", function() {
+    // Substitua SEU_NUMERO_DE_TELEFONE_AQUI pelo número de telefone desejado
+    const numeroDeTelefone = "+5519996171190";
+    const url = `https://api.whatsapp.com/send?phone=${+5519996171190}`;
+    window.location.href = url;
+});
